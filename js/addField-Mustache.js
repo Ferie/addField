b50d83358e3baccfb2a3f6660cb1945666da40a7
Riccardo Andreatta <riccardo.andreatta@myoffersltd.co.uk>
2015-08-19 16:36:10 +0100

    spaces instead of tabs

--- js/addField-Mustache.js
+++ js/addField-Mustache.js
@@ -1,65 +1,65 @@
 $(document).ready(function () {
-	var elecInputCount = 0,
-		gasInputCount = 0;
+    var elecInputCount = 0,
+        gasInputCount = 0;
 
-	$('.addFieldContainer').on('click', function (event) {
-		var $this = $(this),
-			$thisParents = $this.parents('.energyFieldsContainer'),
-			max_fields = 10,
-			counter = 0,
-			prefix,
-			$wrapper;
+    $('.addFieldContainer').on('click', function (event) {
+        var $this = $(this),
+            $thisParents = $this.parents('.energyFieldsContainer'),
+            max_fields = 10,
+            counter = 0,
+            prefix,
+            $wrapper;
 
-		event.preventDefault();
+        event.preventDefault();
 
-		if ($thisParents.hasClass('elec-usage-amount')) {
-			$wrapper = $thisParents.find('.electricityMeter');
-			prefix = 'elec';
-			elecInputCount = elecInputCount + 1;
-			counter = elecInputCount;
-		} else if ($thisParents.hasClass('gas-usage-amount')) {
-			$wrapper = $thisParents.find('.gasMeter');
-			prefix = 'gas';
-			gasInputCount = gasInputCount + 1;
-			counter = gasInputCount;
-		}
+        if ($thisParents.hasClass('elec-usage-amount')) {
+            $wrapper = $thisParents.find('.electricityMeter');
+            prefix = 'elec';
+            elecInputCount = elecInputCount + 1;
+            counter = elecInputCount;
+        } else if ($thisParents.hasClass('gas-usage-amount')) {
+            $wrapper = $thisParents.find('.gasMeter');
+            prefix = 'gas';
+            gasInputCount = gasInputCount + 1;
+            counter = gasInputCount;
+        }
 
-		if (counter <= max_fields) {
-			/* Mustache.js template rendering */
-			var view = {
-				counter: counter,
-				prefix: prefix
-			};
+        if (counter <= max_fields) {
+            /* Mustache.js template rendering */
+            var view = {
+                counter: counter,
+                prefix: prefix
+            };
 
-			var output = Mustache.render($('#addFieldTemplate').html(), view);
+            var output = Mustache.render($('#addFieldTemplate').html(), view);
 
-			$wrapper.append(output);
-			/* End Mustache.js template rendering */
+            $wrapper.append(output);
+            /* End Mustache.js template rendering */
 
-			$wrapper.find('[data-' + prefix + '-field-remove="' + (counter - 1) + '"]').addClass('is-hidden');
+            $wrapper.find('[data-' + prefix + '-field-remove="' + (counter - 1) + '"]').addClass('is-hidden');
 
-			if (counter == max_fields) {
-				$wrapper.siblings('.addFieldContainer').addClass('is-hidden');
-			}
-		}
+            if (counter == max_fields) {
+                $wrapper.siblings('.addFieldContainer').addClass('is-hidden');
+            }
+        }
 
-		$wrapper.find('.removeField[data-' + prefix + '-field-remove="' + counter + '"]').on('click', function (e) {
-			var $this = $(this);
+        $wrapper.find('.removeField[data-' + prefix + '-field-remove="' + counter + '"]').on('click', function (e) {
+            var $this = $(this);
 
-			e.preventDefault();
-			e.stopPropagation();
-			$this.parent().remove();
+            e.preventDefault();
+            e.stopPropagation();
+            $this.parent().remove();
 
-			if (prefix == 'elec') {
-				elecInputCount = elecInputCount - 1;
-				counter = elecInputCount;
-			} else if (prefix == 'gas') {
-				gasInputCount = gasInputCount - 1;
-				counter = gasInputCount;
-			}
+            if (prefix == 'elec') {
+                elecInputCount = elecInputCount - 1;
+                counter = elecInputCount;
+            } else if (prefix == 'gas') {
+                gasInputCount = gasInputCount - 1;
+                counter = gasInputCount;
+            }
 
-			$wrapper.find('[data-' + prefix + '-field-remove="' + counter + '"]').removeClass('is-hidden');
-			$wrapper.siblings('.addFieldContainer').removeClass('is-hidden');
-		});
-	});
+            $wrapper.find('[data-' + prefix + '-field-remove="' + counter + '"]').removeClass('is-hidden');
+            $wrapper.siblings('.addFieldContainer').removeClass('is-hidden');
+        });
+    });
 });
--- js/jquery.addField.js
+++ js/jquery.addField.js
@@ -1,76 +1,76 @@
 ;
 (function ($, window, document, undefined) {
-	"use strict";
+    "use strict";
 
-	var pluginName = 'addField',
-		defaults = {
-			addFieldContainerClass: 'addFieldContainer',
-			addFieldClass: 'addField',
-			generalMultipleFieldsContainerParentsClass: 'generalMultipleFieldsContainer',
-			firstMultipleFieldsContainerParentsClass: 'firstMultipleFieldsContainer',
-			firstWrapperClass: 'firstWrapper',
-			firstPrefix: 'firstPrefix',
-			secondMultipleFieldsContainerParentsClass: null,
-			secondWrapperClass: null,
-			secondPrefix: null,
-			thirdMultipleFieldsContainerParentsClass: null,
-			thirdWrapperClass: null,
-			thirdPrefix: null,
-			fourthMultipleFieldsContainerParentsClass: null,
-			fourthWrapperClass: null,
-			fourthPrefix: null,
-			fifthMultipleFieldsContainerParentsClass: null,
-			fifthWrapperClass: null,
-			fifthPrefix: null,
-			sixthMultipleFieldsContainerParentsClass: null,
-			sixthWrapperClass: null,
-			sixthPrefix: null,
-			mustacheTemplateId: 'addFieldTemplate',
-			maxFieldsNumber: 10
-		};
+    var pluginName = 'addField',
+        defaults = {
+            addFieldContainerClass: 'addFieldContainer',
+            addFieldClass: 'addField',
+            generalMultipleFieldsContainerParentsClass: 'generalMultipleFieldsContainer',
+            firstMultipleFieldsContainerParentsClass: 'firstMultipleFieldsContainer',
+            firstWrapperClass: 'firstWrapper',
+            firstPrefix: 'firstPrefix',
+            secondMultipleFieldsContainerParentsClass: null,
+            secondWrapperClass: null,
+            secondPrefix: null,
+            thirdMultipleFieldsContainerParentsClass: null,
+            thirdWrapperClass: null,
+            thirdPrefix: null,
+            fourthMultipleFieldsContainerParentsClass: null,
+            fourthWrapperClass: null,
+            fourthPrefix: null,
+            fifthMultipleFieldsContainerParentsClass: null,
+            fifthWrapperClass: null,
+            fifthPrefix: null,
+            sixthMultipleFieldsContainerParentsClass: null,
+            sixthWrapperClass: null,
+            sixthPrefix: null,
+            mustacheTemplateId: 'addFieldTemplate',
+            maxFieldsNumber: 10
+        };
 
-	//constructor
-	function plugin(element, options) {
-		this.elem = element;
-		this.$elem = $(element);
-		this.options = $.extend({}, defaults, options);
+    //constructor
+    function plugin(element, options) {
+        this.elem = element;
+        this.$elem = $(element);
+        this.options = $.extend({}, defaults, options);
 
-		this.$addField = this.$elem.find('.' + this.options.addFieldClass);
-		this.elecInputCount = 1;
-		this.gasInputCount = 1;
+        this.$addField = this.$elem.find('.' + this.options.addFieldClass);
+        this.elecInputCount = 1;
+        this.gasInputCount = 1;
 
-		this._defaults = defaults;
-		this._name = pluginName;
+        this._defaults = defaults;
+        this._name = pluginName;
 
-		this.init();
-	}
+        this.init();
+    }
 
-	plugin.prototype = {
-		constructor: plugin,
-		addField: function (event) {
-			var pluginThis = event.data.pluginThis,
-				$this = $(this),
-				$thisParents = $this.parents('.' + pluginThis.options.generalMultipleFieldsContainerParentsClass),
-				max_fields = pluginThis.options.maxFieldsNumber,
-				counter = 0,
-				prefix,
-				$wrapper;
+    plugin.prototype = {
+        constructor: plugin,
+        addField: function (event) {
+            var pluginThis = event.data.pluginThis,
+                $this = $(this),
+                $thisParents = $this.parents('.' + pluginThis.options.generalMultipleFieldsContainerParentsClass),
+                max_fields = pluginThis.options.maxFieldsNumber,
+                counter = 0,
+                prefix,
+                $wrapper;
 
-			event.preventDefault();
+            event.preventDefault();
 
-			if ($thisParents.hasClass(pluginThis.options.firstMultipleFieldsContainerParentsClass)) {
-				$wrapper = $thisParents.find('.' + pluginThis.options.firstWrapperClass);
-				prefix = pluginThis.options.firstPrefix;
-				pluginThis.elecInputCount = pluginThis.elecInputCount + 1;
-				counter = pluginThis.elecInputCount;
-			} else if ($thisParents.hasClass(pluginThis.options.secondMultipleFieldsContainerParentsClass)) {
-				$wrapper = $thisParents.find('.' + pluginThis.options.secondWrapperClass);
-				prefix = pluginThis.options.secondPrefix;
-				pluginThis.gasInputCount = pluginThis.gasInputCount + 1;
-				counter = pluginThis.gasInputCount;
-			}
+            if ($thisParents.hasClass(pluginThis.options.firstMultipleFieldsContainerParentsClass)) {
+                $wrapper = $thisParents.find('.' + pluginThis.options.firstWrapperClass);
+                prefix = pluginThis.options.firstPrefix;
+                pluginThis.elecInputCount = pluginThis.elecInputCount + 1;
+                counter = pluginThis.elecInputCount;
+            } else if ($thisParents.hasClass(pluginThis.options.secondMultipleFieldsContainerParentsClass)) {
+                $wrapper = $thisParents.find('.' + pluginThis.options.secondWrapperClass);
+                prefix = pluginThis.options.secondPrefix;
+                pluginThis.gasInputCount = pluginThis.gasInputCount + 1;
+                counter = pluginThis.gasInputCount;
+            }
 
-			if (counter <= max_fields) {
+            if (counter <= max_fields) {
 //				$wrapper.append('\
 //					<div class="meterReadingFields" data-' + prefix + '-input-count="' + counter + '">\
 //						<div class="meterReadingField">\
@@ -87,64 +87,64 @@
 //					</div>\
 //				');
 
-				/* Mustache.js template rendering */
-				var view = {
-					counter: counter,
-					prefix: prefix
-				};
+                /* Mustache.js template rendering */
+                var view = {
+                    counter: counter,
+                    prefix: prefix
+                };
 
-				var output = Mustache.render($('#' + pluginThis.options.mustacheTemplateId).html(), view);
+                var output = Mustache.render($('#' + pluginThis.options.mustacheTemplateId).html(), view);
 
-				$wrapper.append(output);
-				/* End Mustache.js template rendering */
-				
-				$wrapper.find('[data-' + prefix + '-field-remove="' + (counter-1) + '"]').addClass('is-hidden');
+                $wrapper.append(output);
+                /* End Mustache.js template rendering */
 
-				if (counter == max_fields) {
-					$wrapper.siblings('.' + pluginThis.options.addFieldContainerClass).addClass('is-hidden');
-				}
-			}
+                $wrapper.find('[data-' + prefix + '-field-remove="' + (counter - 1) + '"]').addClass('is-hidden');
 
-			$wrapper.find('.removeField[data-' + prefix + '-field-remove="' + counter + '"]').on('click', function (e) {
-				var $this = $(this);
+                if (counter == max_fields) {
+                    $wrapper.siblings('.' + pluginThis.options.addFieldContainerClass).addClass('is-hidden');
+                }
+            }
 
-				e.preventDefault();
-				e.stopPropagation();
-				$this.parent().remove();
+            $wrapper.find('.removeField[data-' + prefix + '-field-remove="' + counter + '"]').on('click', function (e) {
+                var $this = $(this);
 
-				if (prefix == pluginThis.options.firstPrefix) {
-					pluginThis.elecInputCount = pluginThis.elecInputCount - 1;
-					counter = pluginThis.elecInputCount;
-				} else if (prefix == pluginThis.options.secondPrefix) {
-					pluginThis.gasInputCount = pluginThis.gasInputCount - 1;
-					counter = pluginThis.gasInputCount;
-				}
+                e.preventDefault();
+                e.stopPropagation();
+                $this.parent().remove();
 
-				$wrapper.find('[data-' + prefix + '-field-remove="' + counter + '"]').removeClass('is-hidden');
-				$wrapper.siblings('.' + pluginThis.options.addFieldContainerClass).removeClass('is-hidden');
-			});
-		},
-		eventBindings: function () {
-			this.$addField.on('click', {pluginThis: this}, this.addField);
-		},
-		init: function () {
-			this.eventBindings();
-		}
-	};
+                if (prefix == pluginThis.options.firstPrefix) {
+                    pluginThis.elecInputCount = pluginThis.elecInputCount - 1;
+                    counter = pluginThis.elecInputCount;
+                } else if (prefix == pluginThis.options.secondPrefix) {
+                    pluginThis.gasInputCount = pluginThis.gasInputCount - 1;
+                    counter = pluginThis.gasInputCount;
+                }
 
-	$.fn[pluginName] = function (option) {
-		return this.each(function () {
-			var data = $.data(this, 'ObjPlugin_' + pluginName),
-				options = typeof option === 'object' && option;
+                $wrapper.find('[data-' + prefix + '-field-remove="' + counter + '"]').removeClass('is-hidden');
+                $wrapper.siblings('.' + pluginThis.options.addFieldContainerClass).removeClass('is-hidden');
+            });
+        },
+        eventBindings: function () {
+            this.$addField.on('click', {pluginThis: this}, this.addField);
+        },
+        init: function () {
+            this.eventBindings();
+        }
+    };
 
-			if (!data) $.data(this, 'ObjPlugin_' + pluginName, (data = new plugin(this, options)));
-			if (typeof option === 'string') data[option]();
-		})
-	};
+    $.fn[pluginName] = function (option) {
+        return this.each(function () {
+            var data = $.data(this, 'ObjPlugin_' + pluginName),
+                options = typeof option === 'object' && option;
 
-	$.fn[pluginName].defaults = defaults;
-	$.fn[pluginName].constructor = plugin;
+            if (!data) $.data(this, 'ObjPlugin_' + pluginName, (data = new plugin(this, options)));
+            if (typeof option === 'string') data[option]();
+        });
+    };
 
-	$(document).addField();
+    $.fn[pluginName].defaults = defaults;
+    $.fn[pluginName].constructor = plugin;
+
+    $(document).addField();
 
 })(jQuery, window, document);
